3272638981b3f5ddc7c7fd410e3c46c33cde0c05
Alex Eschenbrenner <alexander.eschenbrenner@perfood.de>
2022-01-12 07:00:28 +0100

    Remove Typings

--- src/definitions.ts
+++ src/definitions.ts
@@ -14,42 +14,8 @@ declare global {
 export interface CapacitorHealthkitPlugin {
   requestAuthorization(): Promise<any>;
   isAvailable(): Promise<any>;
-  queryHKitSampleType(): Promise<QueryOutput>;
+  queryHKitSampleType(): Promise<any>;
   multipleQueryHKitSampleType(): Promise<any>;
   isEditionAuthorized(): Promise<any>;
   multipleIsEditionAuthorized(): Promise<any>;
 }
-
-export interface QueryOutput {
-  countReturn: Number;
-  resultData: SleepData[] | ActivityData[] | StepEnergyData[];
-}
-
-export interface BaseData {
-  startDate: String;
-  endDate: String;
-  source: String;
-  uuid: String;
-  sourceBundleId: String;
-}
-export interface SleepData extends BaseData  {
-  sleepState: String;
-  duration: Number;
-  timeZone: String;
-};
-
-export interface ActivityData extends BaseData {
-  totalFlightsClimbed: Number;
-  totalSwimmingStrokeCount: Number;
-  totalEnergyBurned: Number;
-  totalDistance: Number;
-  workoutActivityId: Number;
-  workoutActivityName: Number;
-  duration: Number;
-};
-
-export interface StepEnergyData extends BaseData {
-  unitName: String; //count|kilocalories
-  duration: Number;
-  value: Number;
-};
