8967fdbad4c74447552242d80db00d66ef5c36a1
Alex Eschenbrenner <alexander.eschenbrenner@perfood.de>
2022-01-14 10:23:24 +0100

    Update typings

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,61 @@
 export interface CapacitorHealthkitPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
-  requestAuthorization(a:any): Promise<void>;
-  queryHKitSampleType(example:any): Promise<any>;
+  requestAuthorization(authOptions: AuthorizationQueryOptions): Promise<void>;
+  queryHKitSampleType<T>(queryOptions:SingleQueryOptions): Promise<QueryOutput<T>>;
   isAvailable(): Promise<void>;
-  multipleQueryHKitSampleType(example:any): Promise<any>;
+  multipleQueryHKitSampleType(queryOptions:MultipleQueryOptions): Promise<any>;
   isEditionAuthorized(): Promise<void>;
   multipleIsEditionAuthorized(): Promise<void>;
 }
+export interface QueryOutput<T = SleepData | ActivityData | OtherData> {
+  countReturn: number;
+  resultData: T[];
+}
+
+export interface BaseData {
+  startDate: string;
+  endDate: string;
+  source: string;
+  uuid: string;
+  sourceBundleId: string;
+  duration: number;
+}
+export interface SleepData extends BaseData  {
+  sleepState: string;
+  timeZone: string;
+}
+
+export interface ActivityData extends BaseData {
+  totalFlightsClimbed: number;
+  totalSwimmingStrokeCount: number;
+  totalEnergyBurned: number;
+  totalDistance: number;
+  workoutActivityId: number;
+  workoutActivityName: number;
+}
+
+export interface OtherData extends BaseData {
+  unitName: string; 
+  value: number;
+}
+
+export interface BaseQueryOptions {
+  startDate: string;
+  endDate: string;
+  limit: number;
+}
+
+export interface SingleQueryOptions extends BaseQueryOptions {
+  sampleName: string;
+}
+
+export interface MultipleQueryOptions extends BaseQueryOptions {
+  sampleNames: string[];
+}
+
+export interface AuthorizationQueryOptions {
+  read: string[];
+  write: string[];
+  all: string[];
+}
+
